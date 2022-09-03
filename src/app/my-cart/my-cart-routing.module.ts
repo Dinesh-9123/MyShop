60526608b835925a350a2a10553ad4f996b53d90
Dinesh Choudhary <dinesh.choudhary@abjima.com>
2022-09-03 18:32:54 +0530

    Update my-cart-routing.module.ts
    add new features

--- src/app/my-cart/my-cart-routing.module.ts
+++ src/app/my-cart/my-cart-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from '../guards/auth-guard.service';
 import { MycartComponent } from './mycart/mycart.component';
 import { OrderDetailsComponent } from './order-details/order-details.component';
 import { ShippingAddressComponent } from './shipping-address/shipping-address.component';
 import { ShippingFormComponent } from './shipping-form/shipping-form.component';
 
 const routes: Routes = [
-  {path:'',component:MycartComponent},
-  {path: 'shipping-form',component:ShippingFormComponent},
-  {path: 'shipping-address',component:ShippingAddressComponent},
-  {path: 'orders', component:OrderDetailsComponent}
+  {path:'',component:MycartComponent, canActivate:[AuthGuardService]},
+  {path: 'shipping-form',component:ShippingFormComponent, canActivate:[AuthGuardService]},
+  {path: 'shipping-address',component:ShippingAddressComponent, canActivate:[AuthGuardService]},
+  {path: 'orders', component:OrderDetailsComponent, canActivate:[AuthGuardService]}
 ];
 
 @NgModule({
